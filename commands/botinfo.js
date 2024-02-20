48543b5ea2db8ae51096a7187eb0f77e5773405f
Xiuhcoatl <mikela.richardson10@gmail.com>
2024-02-20 00:34:32 -0500

    fix formatting

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -21,7 +21,7 @@ module.exports = {
         infoEmbed.setDescription(`Hello, I'm Una! I am a bot that can do lots of different types of role management.\nI have something that every server can use! You can find all of my commands in \`/help\` and you can join my [support server](https://discord.gg/ReBB9fU8ZQ) if you have any questions about me!`);
         infoEmbed.addFields(
             { name: `**Creation Date:**`, value: `I was born on\nAugust 6th, 2021!`, inline: true},
-            { name: `**Creators:**`, value: `My parents are \nxiuhcoatlslayer and faithie`, inline: true},
+            { name: `**Creators:**`, value: `My parents are \nxiuhcoatlslayer and faithie!`, inline: true},
             { name: `**Uptime:**`, value: `I've been awake for ${uptime}!`, inline: false},
             { name: `**Current Servers:**`, value: `I have friends in ${client.guilds.cache.size} servers!`, inline: true},
             { name: `**Privacy Policy and Terms of Service:**`, value: `Read my Privacy Policy and TOS on my [super cool totally awesome website](https://unabot.xyz)!`, inline: false}
